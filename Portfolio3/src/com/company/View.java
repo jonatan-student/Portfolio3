8f5a2586767ea00340effe90410dfcbcb7490f06
Soran98 <soran@ruc.dk>
2022-04-29 22:50:44 +0200

    VIEW DONE

--- Portfolio3/src/com/company/View.java
+++ Portfolio3/src/com/company/View.java
@@ -17,21 +17,24 @@ import javafx.stage.Stage;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-
+//used to create a complete view for all needed elements of the stage
 public class View {
     private Controller controller;
     public Pane view;
 
+    //all buttons that are needed for the system
     Button HomeBtn = new Button("HomePage");
     Button CoursesBtn = new Button("Courses");
     Button TeachersBtn = new Button("Teachers");
     Button StudentsBtn = new Button("Students");
     Button SelectBtn = new Button("Select");
 
+    //Creating the text area that print our information for students, teachers, and courses
     TextArea DisplayInfo = new TextArea();
     ComboBox<String> Info = new ComboBox<>();
 
 
+    //Sets elements visible or not from the controller
     public View(Controller control){
         this.controller = control;
         HomeBtn.setVisible(false);
@@ -41,12 +44,15 @@ public class View {
         Create();
     }
 
+    //creates the entire system and all elements made
     public void Create(){
+        //makes the initial screen and fonts for buttons
         this.view = new Pane();
         Font font = Font.font("Comic Sans MS", FontWeight.BOLD, 36);
         this.view.setMinSize(750,500);
         this.view.setStyle("-fx-border-color:rgba(0,0,0); -fx-border-width:20px; -fx-background-color:rgba(245,222,179, 0.87);");
 
+        //sets position and fonts for initial buttons
         CoursesBtn.relocate(300,200);
         CoursesBtn.setFont(font);
         TeachersBtn.relocate(300, 50);
@@ -65,6 +71,7 @@ public class View {
         DisplayInfo.setEditable(false);
         DisplayInfo.setMinSize(710,410);
 
+        //access the "children" of a node
         this.view.getChildren().add(CoursesBtn);
         this.view.getChildren().add(TeachersBtn);
         this.view.getChildren().add(StudentsBtn);
@@ -78,7 +85,7 @@ public class View {
         this.view.getChildren().add(DisplayInfo);
     }
 
-
+    //To cast the pane node as a parent node for the main class to run
     public Parent asParent(){
         return this.view;
     }
